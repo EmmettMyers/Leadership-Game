bc96a91eebd413a2c7a18c2bd34fdecae1c88bd4
landenf <landenfogle@gmail.com>
2024-04-02 22:50:57 -0500

    Fetch Data

--- frontend/src/components/Leaderboard.jsx
+++ frontend/src/components/Leaderboard.jsx
@@ -1,10 +1,30 @@
 import React, { useState, useEffect } from 'react';
+import { database } from '../firebase';
+import { runTransaction, ref, onValue } from 'firebase/database';
 
 const Leaderboard = () => {
     const [emmettScore, setEmmettScore] = useState(0);
     const [landenScore, setLandenScore] = useState(0);
     const [harleyScore, setHarleyScore] = useState(0);
     const [peytonScore, setPeytonScore] = useState(0);
+
+  useEffect(() => {
+    const usersRef = ref(database, '/Teams');
+    const unsubscribe = onValue(usersRef, snapshot => {
+      if (snapshot.exists()) {
+        setEmmettScore(snapshot.val()['Emmett']);
+        setLandenScore(snapshot.val()['Landen']);
+        setHarleyScore(snapshot.val()['Harley']);
+        setPeytonScore(snapshot.val()['Peyton']);
+      } else {
+        console.log("none");
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+
 
     return (
         <div className="leaderboardPage">
