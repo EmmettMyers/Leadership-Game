4d75b7784889278be653d7ac3e1394e90fa49295
Emmett Myers <emmettleemyers@gmail.com>
2024-04-02 22:13:06 -0500

    Home and leaderboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,55 @@
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  const [page, setPage] = useState("home");
+
+  const handleLeaderClick = (leader) => {
+    setPage("game");
+  };
+
+  const handleLeaderboardClick = () => {
+    setPage("leaderboard");
+  };
+
   return (
     <div className="App">
+
+      {page === "home" && (
+        <div className="home">
+          <div className="container">
+            <div className="joinTeamTxt">Join a team:</div>
+            <div className="leaders">
+              <div className="leader emmett" onClick={() => handleLeaderClick("Emmett")}>
+                Emmett
+              </div>
+              <div className="leader landen" onClick={() => handleLeaderClick("Landen")}>
+                Landen
+              </div>
+              <div className="leader harley" onClick={() => handleLeaderClick("Harley")}>
+                Harley
+              </div>
+              <div className="leader peyton" onClick={() => handleLeaderClick("Peyton")}>
+                Peyton
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {page === "game" && (
+        <div className="game">
+
+        </div>
+      )}
+
+      {page === "leaderboard" && (
+        <div className="leaderboard">
+
+        </div>
+      )}
+
+      <div class="leaderboardLink" onClick={handleLeaderboardClick}>Leaderboard</div>
     </div>
   );
 }
